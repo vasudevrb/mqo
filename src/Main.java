49bfc30ab3d56a8d5124755619c2778c1a1587f3
vasudevrb <47972310+vasudevrb@users.noreply.github.com>
2021-11-03 22:41:20 +0100

    Hide logger warnings

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,29 @@
 import common.Configuration;
 
+import java.io.PrintStream;
+
 public class Main {
 
 
     public static void main(String[] args) throws Exception {
+        hideLoggerWarnings();
+
         Configuration config = Configuration.initialize();
         Tester tester = new Tester(config);
 
         tester.testMVSubstitution();
 //        tester.testBatch();
+    }
+
+    public static void hideLoggerWarnings() {
+        PrintStream filterOut = new PrintStream(System.err) {
+            public void println(String l) {
+                if (!l.startsWith("SLF4J")) {
+                    super.println(l);
+                }
+            }
+        };
+        System.setErr(filterOut);
     }
 
 }
